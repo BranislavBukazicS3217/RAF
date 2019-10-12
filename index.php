6650bd453f8c64bcf828dff83e7dfff6c02f861b
BranislavBukazicS3217 <56486596+BranislavBukazicS3217@users.noreply.github.com>
2019-10-12 20:31:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Input</title>
+</head>
+<body>
+    <form action="output.php" method="get">
+        <p> Ime:<input type="text" name="ime" placeholder="Unesite vase ime"></p>
+        <p> Prezime:<input type="text" name="prezime" placeholder="Unesite vase prezime"></p>
+        <p> Broj idexa: <input type="number" name="broj"></p>
+        <hr>
+        <p><input type="submit" value="Posalji"></p>
+    </form>
+    
+</body>
+</html>
---
+++ output.php
@@ -0,0 +1,9 @@
+<?php
+    $ime = $_GET['ime'];
+    $prezime = $_GET['prezime'];
+    $broj = $_GET['broj'];
+
+    echo 'Ime:'.$ime;
+    echo '<br>Prezime:'.$prezime;
+    echo '<br>Broj indexa:'.$broj;
+?>
